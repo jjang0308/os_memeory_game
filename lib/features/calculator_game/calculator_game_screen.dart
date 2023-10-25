6a8c125d58032e7da6ee9fae88c849f04d410087
Taewoo-03 <kty44552@naver.com>
2023-10-25 23:19:30 +0900

    버튼 생성

--- lib/features/calculator_game/calculator_game_screen.dart
+++ lib/features/calculator_game/calculator_game_screen.dart
@@ -13,25 +13,23 @@ class _CarculatorGameScreenState extends State<CarculatorGameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
-        backgroundColor: Colors.deepOrange,
-        foregroundColor: Colors.white,
-        title: const Text(
-          '깜짝게임 : 가격을 맞춰봐~!',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 2,
+          backgroundColor: Colors.deepOrange,
+          foregroundColor: Colors.white,
+          title: const Text(
+            '깜짝게임 : 가격을 맞춰봐~!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
         ),
-      ),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        Column(
+        body: Column(
           children: [
+            const SizedBox(
+              height: 50,
+            ),
             Padding(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 40), // 양쪽으로 10 픽셀씩 외부 패딩 주기
+              padding: const EdgeInsets.symmetric(horizontal: 50),
               child: ElevatedButton(
                 onPressed: () {
                   setState(() {
@@ -40,7 +38,7 @@ class _CarculatorGameScreenState extends State<CarculatorGameScreen> {
                 },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.deepOrange,
-                  minimumSize: const Size(200, 150),
+                  minimumSize: const Size(200, 200), // 가로와 세로 크기를 같게 설정
                   padding: const EdgeInsets.all(20),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(15),
@@ -58,9 +56,100 @@ class _CarculatorGameScreenState extends State<CarculatorGameScreen> {
                 ),
               ),
             ),
+            const SizedBox(
+              height: 50, // 버튼 사이의 간격 조절
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    calcul += 500;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                  minimumSize: const Size(200, 200), // 가로와 세로 크기를 같게 설정
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40),
+                  child: Text(
+                    '버튼 2',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50, // 버튼 사이의 간격 조절
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    calcul += 500;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                  minimumSize: const Size(200, 200), // 가로와 세로 크기를 같게 설정
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40),
+                  child: Text(
+                    '버튼 3',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50, // 버튼 사이의 간격 조절
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    calcul += 500;
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepOrange,
+                  minimumSize: const Size(200, 200), // 가로와 세로 크기를 같게 설정
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 40),
+                  child: Text(
+                    '버튼 4',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 70,
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
-        ),
-      ]),
-    );
+        ));
   }
 }
