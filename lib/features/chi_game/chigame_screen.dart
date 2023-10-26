25f6ec4d68d7bbf8e780acf373be07d1d3a64d9a
jjang0308 <jang653838@naver.com>
2023-10-26 19:42:02 +0900

    꼬치게임 코드추가

--- lib/features/chi_game/chigame_screen.dart
+++ lib/features/chi_game/chigame_screen.dart
@@ -6,17 +6,26 @@ class ChiGameScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-       backgroundColor: const Color(0xFFF79824),
-       title: const Text("00네 \n꼬치꼬치",
-       style: TextStyle(
-        fontFamily: 'Soyo-Maple-REGULAR',
-        fontSize: 100,
-       ),),
-
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(300.0), // AppBar의 높이 조절
+        child: AppBar(
+          elevation: 0,
+          backgroundColor: const Color(0xFFF79824),
+          flexibleSpace: Container(
+            padding: const EdgeInsets.only(bottom: 24.0), // 텍스트와 아래 여백 설정
+            alignment: Alignment.bottomCenter, // 텍스트를 아래 정렬
+            child: const Text(
+              "00네\n꼬치꼬치",
+              style: TextStyle(
+                fontFamily: 'Soyo-Maple-BOLD',
+                fontSize: 100, // 폰트 크기 조정
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
       ),
       backgroundColor: const Color(0xFFF79824),
-      
     );
   }
 }
