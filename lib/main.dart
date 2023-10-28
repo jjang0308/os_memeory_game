f41a77e838c5b40dcf58b4f7b16924728764129a
parkchanwu <116543427+parkchanwu@users.noreply.github.com>
2023-10-28 15:38:31 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:os_memory_game/features/home/home_screen.dart';
 
 void main() {
-  // dbConnector();
+  // 네이티브 코드를 사용하는 경우, 플랫폼간의 통신을 함->비동기적이벤트를 사용하는 경우 사용
+  WidgetsFlutterBinding.ensureInitialized;
+
   runApp(const MyApp());
 }
 
