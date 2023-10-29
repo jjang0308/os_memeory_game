644a8aa52ee5b6c8c1c80a1737f111e803d5c6bd
jjang0308 <jang653838@naver.com>
2023-10-29 23:41:52 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:os_memory_game/features/chi_game/chigame_screen.dart';
-import 'package:os_memory_game/features/home/home_screen.dart';
+
 
 void main() {
   // 네이티브 코드를 사용하는 경우, 플랫폼간의 통신을 함->비동기적이벤트를 사용하는 경우 사용
