c5d50d578eb28fe182ded27f33bfd12d316a89b6
parkchanwu <116543427+parkchanwu@users.noreply.github.com>
2023-10-30 00:36:42 +0900

    Update

--- lib/features/chi_game/chigame_motion.dart
+++ lib/features/chi_game/chigame_motion.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:animate_do/animate_do.dart';
 
 void main() => runApp(const NewContainer());
 
 class NewContainer extends StatelessWidget {
   const NewContainer({super.key});
- 
+
   @override
-  Widget build(BuildContext context) { 
-    return Container(height: 60, width: 60, color: Colors.green); 
-  } 
+  Widget build(BuildContext context) {
+    return Container(height: 60, width: 60, color: Colors.green);
+  }
 }
