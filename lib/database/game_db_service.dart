9c47923bc3051fcff41c11407fc676b7ac0f6b46
parkchanwu <116543427+parkchanwu@users.noreply.github.com>
2023-10-30 00:33:28 +0900

    UpDate

--- lib/database/game_db_service.dart
+++ lib/database/game_db_service.dart
@@ -2,6 +2,7 @@ import 'package:os_memory_game/model/game_model.dart';
 
 import 'game_db_query.dart';
 
+//Update
 class GameInfoDBService {
   ///GameDBQuery을 이용하여 db에서 값을 가져옴
   static Future<List<GameModel>> getGameInfo() {
