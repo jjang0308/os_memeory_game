b2c70f3b18ee251296beaed89ac3e6475b9b05dc
jjang0308 <jang653838@naver.com>
2023-10-30 23:00:07 +0900

    다른버튼을 누르면 그버튼의 이미지 가 꽂히는거 구현

--- lib/features/chi_game/chigame_motion.dart
+++ lib/features/chi_game/chigame_motion.dart
@@ -7,9 +7,8 @@ class ChiGameMotion extends StatefulWidget {
     final bool isSelected;
     final double positionLeft;
     final double positionTop;
-  const ChiGameMotion({super.key, required this.isSelected, required this.positionLeft, required this.positionTop});
-
-
+    final String imageName;
+  const ChiGameMotion({super.key, required this.isSelected, required this.positionLeft, required this.positionTop, required this.imageName});
 
   @override
   State<ChiGameMotion> createState() => _ChiGameMotionState();
@@ -19,12 +18,15 @@ class _ChiGameMotionState extends State<ChiGameMotion> {
   @override
   Widget build(BuildContext context) {
     return Positioned(
-       top: widget.positionTop
- ,
-            left: widget.positionLeft,
-      child: FadeInDown(from: 100, child: SizedBox(
-        height : 200, width: 200, 
-        child: Image.asset('assets/fonts/images/pimang.png'),),),
+      top: widget.positionTop,
+      left: widget.positionLeft,
+      child: FadeInDown( duration: const Duration(milliseconds: 800) , animate: true,
+        child: SizedBox(
+          height: 200,
+          width: 200,
+          child: Image.asset('assets/fonts/images/${widget.imageName}'),
+        ),
+      ),
     );
   }
 }
