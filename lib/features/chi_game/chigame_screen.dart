fa222b31cdec120913698b022f71773da6c91612
jiheechoe <cch010723@naver.com>
2023-11-01 21:46:21 +0900

    Update

--- lib/features/chi_game/chigame_screen.dart
+++ lib/features/chi_game/chigame_screen.dart
@@ -86,7 +86,8 @@ int price = getRandomPrice();
 
 //스크린 폴더 생성
 class ChiGameScreen extends StatefulWidget {
-  const ChiGameScreen({super.key});
+  final List chiIndex;
+  const ChiGameScreen({super.key, required this.chiIndex});
 
   @override
   State<ChiGameScreen> createState() => _ChiGameScreenState(); //장혁 바보
@@ -94,9 +95,8 @@ class ChiGameScreen extends StatefulWidget {
 
 class _ChiGameScreenState extends State<ChiGameScreen> {
   bool isButtonPressed = false;
-
   String name = '';
-
+  List<int> selectedChi = [];
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
@@ -124,6 +124,8 @@ class _ChiGameScreenState extends State<ChiGameScreen> {
       }
     }
 
+    print(widget.chiIndex);
+    print(selectedChi);
     return Scaffold(
       backgroundColor: Theme.of(context).secondaryHeaderColor,
       body: Stack(
@@ -162,6 +164,7 @@ class _ChiGameScreenState extends State<ChiGameScreen> {
                               onFoodSelected: (selectedImageNames) {
                                 onFoodSelected(
                                     index, buttonImages[index]['imageName']);
+                                selectedChi.add(index);
                               },
                             ),
                           ],
@@ -176,8 +179,11 @@ class _ChiGameScreenState extends State<ChiGameScreen> {
                               imageName: buttonImages[index + 5]['imageName'],
                               isSelected: isSelected,
                               onFoodSelected: (selectedImageNames) {
-                                onFoodSelected(index,
-                                    buttonImages[index + 5]['imageName']);
+                                onFoodSelected(
+                                  index,
+                                  buttonImages[index + 5]['imageName'],
+                                );
+                                selectedChi.add(index + 5);
                               },
                             ),
                           ],
@@ -278,7 +284,7 @@ class _ChiGameScreenState extends State<ChiGameScreen> {
                     selectedImageNames = List.filled(5, '');
                     // print(selectedImageNames.size);
                     // 버튼을 눌렀을 때 HomeScreen으로 이동
-                    Navigator.of(context).push(
+                    Navigator.of(context).pushReplacement(
                       MaterialPageRoute(
                         builder: (context) =>
                             const HomeScreen(), // HomeScreen 위젯으로 이동
@@ -346,6 +352,17 @@ class _ChiGameScreenState extends State<ChiGameScreen> {
                     positionTop: 1050 - (i * 150),
                     imageName: selectedImageNames[i],
                   ),
+              // if (selectedChi.length == widget.chiIndex.length)
+              //   Positioned(
+              //     top: screenHeight * 0.25,
+              //     left: screenWidth * 0.1,
+              //     child: Icon(
+              //       selectedChi.equals(widget.chiIndex)
+              //           ? Icons.panorama_fish_eye
+              //           : Icons.close,
+              //       size: screenWidth * 0.8,
+              //     ),
+              //   ),
             ],
           ),
         ],
--- lib/features/orders/random_next_screen.dart
+++ lib/features/orders/random_next_screen.dart
@@ -1,93 +1,93 @@
-import 'package:flutter/material.dart';
-import 'package:os_memory_game/features/chi_game/chigame_screen.dart';
-import 'package:os_memory_game/features/home/home_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:os_memory_game/features/chi_game/chigame_screen.dart';
+// import 'package:os_memory_game/features/home/home_screen.dart';
 
-class RandomNext extends StatelessWidget {
-  const RandomNext({Key? key}) : super(key: key);
+// class RandomNext extends StatelessWidget {
+//   const RandomNext({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black, // 배경색을 검은색으로 설정
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 1000,
-              height: 300,
-              color: Colors.black,
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    '잘 기억하셨나요?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 100,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Soyo-Maple-Bold",
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    '그럼 알맞게 꽂아주세요!!', // Add your additional text here
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 80, // Adjust the font size as needed
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "Soyo-Maple-Bold",
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // 첫 번째 버튼이 클릭되었을 때 수행할 동작
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const HomeScreen(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: Colors.white, // 버튼의 텍스트 색상을 검은색으로 설정
-                    minimumSize: const Size(500, 100), // 버튼의 최소 크기를 조절
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10), // 내부 패딩을 조절
-                  ),
-                  child: const Text('취소',
-                      style: TextStyle(fontSize: 50)), // 버튼 텍스트 크기를 조절
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // 두 번째 버튼이 클릭되었을 때 수행할 동작
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ChiGameScreen(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
-                    backgroundColor: Colors.white, // 버튼의 텍스트 색상을 검은색으로 설정
-                    minimumSize: const Size(500, 100), // 버튼의 최소 크기를 조절
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10), // 내부 패딩을 조절
-                  ),
-                  child: const Text('다음',
-                      style: TextStyle(fontSize: 50)), // 버튼 텍스트 크기를 조절
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.black, // 배경색을 검은색으로 설정
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Container(
+//               width: 1000,
+//               height: 300,
+//               color: Colors.black,
+//               child: const Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     '잘 기억하셨나요?',
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 100,
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: "Soyo-Maple-Bold",
+//                     ),
+//                   ),
+//                   SizedBox(height: 30),
+//                   Text(
+//                     '그럼 알맞게 꽂아주세요!!', // Add your additional text here
+//                     style: TextStyle(
+//                       color: Colors.white,
+//                       fontSize: 80, // Adjust the font size as needed
+//                       fontWeight: FontWeight.bold,
+//                       fontFamily: "Soyo-Maple-Bold",
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     // 첫 번째 버튼이 클릭되었을 때 수행할 동작
+//                     Navigator.of(context).push(
+//                       MaterialPageRoute(
+//                         builder: (context) => const HomeScreen(),
+//                       ),
+//                     );
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     foregroundColor: Colors.black,
+//                     backgroundColor: Colors.white, // 버튼의 텍스트 색상을 검은색으로 설정
+//                     minimumSize: const Size(500, 100), // 버튼의 최소 크기를 조절
+//                     padding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 10), // 내부 패딩을 조절
+//                   ),
+//                   child: const Text('취소',
+//                       style: TextStyle(fontSize: 50)), // 버튼 텍스트 크기를 조절
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     // 두 번째 버튼이 클릭되었을 때 수행할 동작
+//                     Navigator.of(context).push(
+//                       MaterialPageRoute(
+//                         builder: (context) => ChiGameScreen(),
+//                       ),
+//                     );
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     foregroundColor: Colors.black,
+//                     backgroundColor: Colors.white, // 버튼의 텍스트 색상을 검은색으로 설정
+//                     minimumSize: const Size(500, 100), // 버튼의 최소 크기를 조절
+//                     padding: const EdgeInsets.symmetric(
+//                         horizontal: 20, vertical: 10), // 내부 패딩을 조절
+//                   ),
+//                   child: const Text('다음',
+//                       style: TextStyle(fontSize: 50)), // 버튼 텍스트 크기를 조절
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
